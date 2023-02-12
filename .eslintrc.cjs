a7a354e2f692c8ac545fec05269ea0a26a1ca4f4
João Pedro <jpedro1580@gmail.com>
2023-02-12 14:45:48 -0300

    chore(config): Formatting and linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: ['plugin:react/recommended', 'standard-with-typescript'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json']
   },
   plugins: ['react'],
   rules: {
     semi: 0,
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/semi': 'off',
-    '@typescript-eslint/space-before-function-paren': 'off',
-  },
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
 };
