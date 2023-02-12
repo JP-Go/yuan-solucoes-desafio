c3679a3c7a6d46b475a4f7b238996e95e320096b
João Pedro <jpedro1580@gmail.com>
2023-02-12 16:25:18 -0300

    feat(autocomplete): Setup autocompletion for places in the ui and set props to store them

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
-import { type ReactElement, useMemo } from 'react';
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable multiline-ternary */
+import { type ReactElement, useMemo, useState } from 'react';
 import { InfoOverlay } from './components/InfoOverlay';
 import { SearchBar } from './components/SearchBar';
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { type LatLng } from './@types/interfaces';
+
+const libraries = ['places'];
 
 function App(): ReactElement {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GEOCODING_API_KEY
+    googleMapsApiKey: import.meta.env.VITE_GEOCODING_API_KEY,
+    libraries
   });
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), [])
+  const center = useMemo(() => ({ lat: -5.1, lng: -42.9 }), []);
+
+  const [selected, setSelected] = useState<LatLng[]>([]);
+  const hasSelectedPlaces = selected?.length > 0;
   return (
     <div className="flex flex-col relative w-screen h-screen items-center">
-      {isLoaded
-        ? (
+      {isLoaded ? (
         <>
           <GoogleMap
             zoom={10}
-            center={center}
+            center={selected.length > 0 ? selected.at(-1) : center}
             mapContainerClassName="w-full h-full"
-          ></GoogleMap>
-          <SearchBar />
-          <InfoOverlay />
+          >
+            {hasSelectedPlaces
+              ? selected.map((position) => {
+                  return <Marker key={position.id} position={position} />;
+                })
+              : null}
+          </GoogleMap>
+          <SearchBar
+            onSelected={(location) => {
+              setSelected([...selected, location]);
+            }}
+          />
+          <InfoOverlay
+            title={!hasSelectedPlaces ? 'Últimas rotas' : 'Nova rota'}
+            selectedPlaces={selected}
+          />
         </>
-          )
-        : (
+      ) : (
         <div className="w-20 h-20 border-l-2 border-b-2 border-slate-800 rounded-full animate-spin"></div>
-          )}
+      )}
     </div>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,69 @@
 import { MagnifyingGlass } from 'phosphor-react';
 import { type ReactElement } from 'react';
+import usePlacesAutocomplete from 'use-places-autocomplete';
+import { type LatLng } from '../@types/interfaces';
+import { fetchAddresses } from '../lib/api/geocoding';
 
-export function SearchBar(): ReactElement {
+interface SearchBarProps {
+  onSelected: (location: LatLng) => void;
+}
+
+export function SearchBar({ onSelected }: SearchBarProps): ReactElement {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions
+  } = usePlacesAutocomplete();
+
+  async function handleSelectLocation(placeId: string): Promise<void> {
+    const location = await fetchAddresses(placeId);
+    onSelected({
+      lat: location.lat,
+      lng: location.lng
+    });
+    clearSuggestions();
+  }
+
   return (
-    <div className="relative top-2 h-16 w-3/5 z-10 flex items-center rounded-lg drop-shadow-lg">
-      <MagnifyingGlass
-        className="bg-white text-slate-500 rounded-r-lg h-12 pr-2 drop-shadow-r-lg"
-        size={32}
-        mirrored
-      />
-      <input
-        className="bg-white placeholder:text-slate-500 p-2 w-full h-12 rounded-r-lg outline-none drop-shadow-t-lg drop-shadow-rb"
-        placeholder="Busque um endereço para iniciar uma nova rota"
-      />
+    <div className="absolute top-2 h-16 w-3/5 z-10 flex flex-col items-center rounded-lg drop-shadow-lg">
+      <div className="flex w-full">
+        <MagnifyingGlass
+          className="bg-white text-slate-500 rounded-r-lg h-12 pr-2 drop-shadow-r-lg"
+          size={32}
+          mirrored
+        />
+        <input
+          className="bg-white placeholder:text-slate-500 p-2 w-full h-12 rounded-r-lg outline-none drop-shadow-t-lg drop-shadow-rb"
+          placeholder="Busque um endereço para iniciar uma nova rota"
+          value={value}
+          disabled={!ready}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+      </div>
+      {status === 'OK'
+        ? (
+        <ul className="bg-white flex flex-col z-10 w-full rounded-lg mt-2 p-2 gap-2">
+          {data.map(({ place_id: placeId, description }) => {
+            return (
+              <li
+                className="border-b-2 border-slate-300 max-w-full cursor-pointer"
+                key={placeId}
+                onClick={() => {
+                  setValue(description, false)
+                  void handleSelectLocation(placeId);
+                }}
+              >
+                {description}
+              </li>
+            );
+          })}
+        </ul>
+          )
+        : null}
     </div>
   );
 }
