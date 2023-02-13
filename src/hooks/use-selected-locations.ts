5a8675fd488865b483957b495a0a3f13bb366f7f
João Pedro <jpedro1580@gmail.com>
2023-02-13 12:41:25 -0300

    refactor(hooks): Add functions to reorder locations when creating a route.

--- src/hooks/use-selected-locations.ts
+++ src/hooks/use-selected-locations.ts
@@ -8,6 +8,33 @@ export function useLocations() {
   function clearLocations() {
     setLocations([]);
   }
+
+  function moveTowardsEnd(location: Location) {
+    const index = locations.findIndex((loc) => loc.id === location.id);
+    if (index === -1) {
+      throw new Error('Localização com id não presente');
+    }
+    if (index === locations.length - 1) {
+      return;
+    }
+    const locationsCopy = [...locations];
+    locationsCopy[index] = locationsCopy[index + 1];
+    locationsCopy[index + 1] = location;
+    setLocations(locationsCopy);
+  }
+  function moveTowardsStart(location: Location) {
+    const index = locations.findIndex((loc) => loc.id === location.id);
+    if (index === -1) {
+      throw new Error('Localização com id não presente');
+    }
+    if (index === 0) {
+      return;
+    }
+    const locationsCopy = [...locations];
+    locationsCopy[index] = locationsCopy[index - 1];
+    locationsCopy[index - 1] = location;
+    setLocations(locationsCopy);
+  }
   function appendLocation(location: Location) {
     setLocations((locs) => [...locs, location]);
   }
@@ -21,6 +48,8 @@ export function useLocations() {
     hasSelectedLocations,
     appendLocation,
     clearLocations,
-    removeLocation
+    removeLocation,
+    moveTowardsEnd,
+    moveTowardsStart
   };
 }
