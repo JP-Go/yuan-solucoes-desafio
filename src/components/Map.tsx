cdee79c07c3f252da6266d931f36d2edc34fc58a
João Pedro <jpedro1580@gmail.com>
2023-02-13 23:36:00 -0300

    feat(directions): Implement showing directions on the map when stops are selected.
    Now when more than one stop is selected, the map shows directions
    to follow such that every stop is reached. When the stops are changed
    the directions are recalculated

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,11 @@
-import { GoogleMap, Marker } from '@react-google-maps/api';
-import { useMemo } from 'react';
+import { DirectionsRenderer, GoogleMap, Marker } from '@react-google-maps/api';
+import { useEffect, useMemo, useState } from 'react';
 import { Location } from '../@types/interfaces';
 
+type Map = google.maps.Map;
+type DirectionsResult = google.maps.DirectionsResult;
 interface MapProps {
-  onLoad: (map: google.maps.Map) => void;
+  onLoad: (map: Map) => void;
   hasSelectedStops: boolean;
   locations: Location[];
 }
@@ -14,6 +16,49 @@ export function Map({ onLoad, locations, hasSelectedStops }: MapProps) {
     return location;
   }, []);
   const zoom = 13;
+  const [directions, setDirections] = useState<DirectionsResult | undefined>(
+    undefined
+  );
+  const shouldFetch = locations.length > 1;
+  const service = new google.maps.DirectionsService();
+
+  useEffect(() => {
+    if (shouldFetch) {
+      setTimeout(() => {
+        fetchDirections(locations);
+      }, 1500);
+    } else {
+      setDirections(undefined);
+    }
+  }, [shouldFetch, locations]);
+
+  async function fetchDirections(locations: Location[]) {
+    if (locations.length < 2) {
+      return;
+    }
+    const firstLocation = locations[0];
+    const lastlocation = locations.at(-1)!;
+    const waypoints =
+      locations.length > 2
+        ? locations.slice(1, -1).map((location) => ({
+            location: location
+          }))
+        : undefined;
+    service.route(
+      {
+        origin: firstLocation,
+        destination: lastlocation,
+        travelMode: google.maps.TravelMode.DRIVING,
+        waypoints
+      },
+      (results, status) => {
+        if (status === 'OK' && results) {
+          setDirections(results);
+        }
+      }
+    );
+  }
+
   return (
     <GoogleMap
       onLoad={onLoad}
@@ -21,6 +66,7 @@ export function Map({ onLoad, locations, hasSelectedStops }: MapProps) {
       center={hasSelectedStops ? locations.at(-1) : center}
       mapContainerClassName="w-full h-full"
     >
+      {directions ? <DirectionsRenderer directions={directions} /> : null}
       {hasSelectedStops &&
         locations.map((location) => {
           return <Marker key={location.id} position={location} />;
