d8d1c53c75cd56a53aeb77b4d75e6267f429d869
João Pedro <jpedro1580@gmail.com>
2023-02-14 00:14:00 -0300

    refactor(components): Extract Stop Info into it's own component

--- src/components/RouteStops.tsx
+++ src/components/RouteStops.tsx
@@ -4,6 +4,7 @@ import { type Location } from '../@types/interfaces';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { SaveRouteButton } from './SaveRouteButton';
 import { useRoutesStore } from '../features/routes-slice';
+import { RouteStopInfo } from './RouteStopInfo';
 
 interface RouteStopsProps {
   stops: Location[];
@@ -11,15 +12,7 @@ interface RouteStopsProps {
 
 export function RouteStops({ stops }: RouteStopsProps): ReactElement {
   const [parent] = useAutoAnimate();
-  const {
-    moveTowardsEnd,
-    moveTowardsStart,
-    removeStop,
-    clearStops,
-    saveRoute,
-    routeId,
-    updateRoute
-  } = useRoutesStore();
+  const { clearStops, saveRoute, routeId, updateRoute } = useRoutesStore();
 
   function handleSaveRoute(stops: Location[]) {
     if (routeId) {
@@ -45,51 +38,13 @@ export function RouteStops({ stops }: RouteStopsProps): ReactElement {
           ? 'Chegando em: '
           : 'Parada em: ';
         return (
-          <div
-            className="flex flex-col items-center gap-2 w-full relative"
+          <RouteStopInfo
             key={location.id}
-          >
-            <div className="text-slate-800 grid grid-cols-6 w-3/4 place-items-center">
-              <div className="col-start-1 col-span-5">
-                <span className="font-bold">{predicateText}</span>
-                <span>
-                  {`${location.displayText.slice(0, 50)}${
-                    location.displayText.length > 50 ? '...' : ''
-                  }`}
-                </span>
-              </div>
-              <div className="flex gap-2 place-self-end">
-                {!firstLocation && (
-                  <CaretUp
-                    weight="fill"
-                    size={24}
-                    className="text-slate-500 cursor-pointer"
-                    onClick={() => {
-                      moveTowardsStart(location);
-                    }}
-                  />
-                )}
-                {!lastLocation && (
-                  <CaretDown
-                    weight="fill"
-                    size={24}
-                    className="text-slate-500 cursor-pointer"
-                    onClick={() => {
-                      moveTowardsEnd(location);
-                    }}
-                  />
-                )}
-                <X
-                  className="text-red-500 cursor-pointer"
-                  size={24}
-                  onClick={() => removeStop(location.id)}
-                />
-              </div>
-            </div>
-            {idx !== stops.length - 1 ? (
-              <ArrowDown size={24} className="text-slate-500" weight="bold" />
-            ) : null}
-          </div>
+            location={location}
+            predicateText={predicateText}
+            firstLocation={firstLocation}
+            lastLocation={lastLocation}
+          />
         );
       })}
       {stops.length > 1 ? (
